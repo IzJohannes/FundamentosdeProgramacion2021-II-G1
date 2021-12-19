30009fda94e72589c84da82b1983b0e3c9c54317
IzJohannes <90219110+IzJohannes@users.noreply.github.com>
2021-12-19 01:37:41 +0000

    Unidad-3

--- ExamenJGVC/src/main/java/pe/edu/upeu/jgvc/App.java
+++ ExamenJGVC/src/main/java/pe/edu/upeu/jgvc/App.java
@@ -1,11 +1,42 @@
 package pe.edu.upeu.jgvc;
-import pe.edu.upeu.jgvc.util.LeerTeclado;
-import pe.edu.upeu.jgvc.examen.ResolucionExamen;
-
+import pe.edu.upeu.jgvc.util.*;
+import pe.edu.upeu.jgvc.examen.*;
 public class App{
-  static LeerTeclado cd = new LeerTeclado();
-  static ResolucionExamen pou = new ResolucionExamen();
-    public static void main( String[] args ){
-     pou.impuestoAutos();
+  static LeerTeclado pou2 = new LeerTeclado();
+  
+  static ResolucionExamen ali = new ResolucionExamen();
+  
+  static void MenudePreguntasdeExamen(){
+
+    int p=pou2.leer(0,"\nPreguntasdeExamen:"+"\nAlgoritmo 2 : 1"+"\nAlgoritmo 3 : 2"+
+        "\nAlgoritmo 4 : 3"+"\nAlgoritmo 5 : 4"+"\nSimbolo de cierre: 0"+"\nElija el Algoritmo");
+        
+    while(p!=0){
+            switch(p){
+            case 1: ali.CompañiaAutomotriz();break;
+
+            case 2: ali.TabladeMultiplicaral20();break;
+                        
+            case 3: ali.NumerosPerfectos();break;
+
+            case 4: int a=pou2.leer(0,"Introdusca el primer valor");
+              int s=pou2.leer(0,"Introdusca el segundo valor");
+    System.out.println("El resultado es: "+ali.Potenciacion(a, s));break;
+                        
+            default: System.out.println("Error");   
+}
+  System.out.println("--------------------------");
+  p=pou2.leer(0,"\nPreguntasdeExamen:"+"\nAlgoritmo 2 : 1"+"\nAlgoritmo 3 : 2"+
+  "\nAlgoritmo 4 : 3"+"\nAlgoritmo 5 : 4"+"\nSimbolo de cierre: c"+"\nElija el Algoritmo");
+}
+
+        }
+public static void main( String[] args ){
+
+System.out.println( "Bienvenido al Sistema de Menu de Opciones" );
+       MenudePreguntasdeExamen();
+       
     }
+
+    
 }
